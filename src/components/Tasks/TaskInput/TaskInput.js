5e7009db3e73eb688edafbf1962a69b035f69a7b
uladzimir <01myit@gmail.com>
2023-08-29 20:41:43 +0300

    condition CSS - inline styles

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,29 +1,49 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
-import Button from "../../UI/Button/Button";
-import "./TaskInput.css";
+import Button from "../../UI/Button/Button"
+import "./TaskInput.css"
 
 const TaskInput = ({ addTaskHandler }) => {
   const [inputText, setInputText] = useState("");
-
-  const taskInputChangeHandler = (event) => {
-    setInputText(event.target.value);
+  const [isValidInput, setIsValidInput] = useState(true)
+  const taskInputChangeHandler = (e) => {
+    setInputText(e.target.value)
   };
 
-  const formSubmitHandler = (event) => {
-    event.preventDefault();
-    addTaskHandler(inputText);
-  };
-
+  const formSubmitHandler = (e) => {
+    e.preventDefault()
+    if (inputText.trim().length === 0) {
+      setIsValidInput(false)
+      setInputText('Not valid value')
+      return
+    }
+    addTaskHandler(inputText)
+    setInputText('')
+    setIsValidInput(true)
+  }
+  function onInputFocus(e) {
+    setIsValidInput(true)
+    setInputText('')
+  }
+  const labelStyle = isValidInput ? 'black' : '#FFC9C9'
+  const inputStyle = isValidInput ?
+    { backgroundColor: '#c8e1e4', borderColor: '#c8e1e4', fontStyle: 'normal', color: 'black' } :
+    { backgroundColor: '#FFC9C9', borderColor: '#FFC9C9', fontStyle: 'italic', color: 'red' }
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
+        <label style={{ color: labelStyle }}>Задачи</label>
+        <input
+          type="text"
+          value={inputText}
+          onFocus={onInputFocus}
+          onChange={taskInputChangeHandler}
+          style={ inputStyle }
+        />
       </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
-  );
-};
+  )
+}
 
 export default TaskInput;
