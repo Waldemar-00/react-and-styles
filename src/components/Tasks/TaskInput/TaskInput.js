396454093fe2597fbd16d057399f7e67ca928bbc
uladzimir <01myit@gmail.com>
2023-08-29 21:44:54 +0300

    dynamic CSS, diableding attribute of button is toggle

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -25,23 +25,19 @@ const TaskInput = ({ addTaskHandler }) => {
     setIsValidInput(true)
     setInputText('')
   }
-  const labelStyle = isValidInput ? 'black' : '#FFC9C9'
-  const inputStyle = isValidInput ?
-    { backgroundColor: '#c8e1e4', borderColor: '#c8e1e4', fontStyle: 'normal', color: 'black' } :
-    { backgroundColor: '#FFC9C9', borderColor: '#FFC9C9', fontStyle: 'italic', color: 'red' }
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: labelStyle }}>Задачи</label>
+      <div className={`form-control ${isValidInput ? '' : 'invalid'}`}>
+        <label>Задачи</label>
         <input
           type="text"
-          value={inputText}
+          value={ inputText }
           onFocus={onInputFocus}
           onChange={taskInputChangeHandler}
-          style={ inputStyle }
         />
       </div>
-      <Button type="submit">Добавить Задачу</Button>
+      <Button disabled={!isValidInput} type="submit">Добавить Задачу</Button>
     </form>
   )
 }
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,8 @@
 import "./Button.css"
 
-const Button = ({ type, children }) => {
+const Button = ({ type, children, disabled }) => {
   return (
-    <button type={type} className="button">
+    <button type={type} className="button" disabled={disabled}>
       {children}
     </button>
   )
