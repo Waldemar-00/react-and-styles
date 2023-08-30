d13aa7a886de58df81ec60e150847a66f0b1f45e
uladzimir <01myit@gmail.com>
2023-08-30 20:58:35 +0300

    used object destructurization in Tagget Template Literals

--- src/components/Tasks/TaskInput/ControlInput.js
+++ src/components/Tasks/TaskInput/ControlInput.js
@@ -7,16 +7,16 @@ const ControlInput = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
-    color: ${props => props.onInvalid ? 'inherit' : '#FFC9C9'};
+    color: ${({ onInvalid }) => onInvalid ? 'inherit' : '#FFC9C9'};
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid ${props => props.onInvalid ? '#ccc' : '#FFC9C9'};
-    background-color: ${props => props.onInvalid ? '#ccc' : '#FFC9C9'};
-    font-style: ${props => props.onInvalid ? 'inherit' : 'italic'};
-    color: ${props => props.onInvalid ? 'inherit' : 'red'};
+    border: 1px solid ${({ onInvalid }) => onInvalid ? '#ccc' : '#FFC9C9'};
+    background-color: ${({ onInvalid }) => onInvalid ? '#ccc' : '#FFC9C9'};
+    font-style: ${({ onInvalid }) => onInvalid ? 'inherit' : 'italic'};
+    color: ${({ onInvalid }) => onInvalid ? 'inherit' : 'red'};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
