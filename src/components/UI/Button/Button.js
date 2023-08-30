2ae5ba0ce7056d3a781cb870a5c492cfface5cd0
uladzimir <01myit@gmail.com>
2023-08-30 20:09:36 +0300

    added styled-components library and used it for build Button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,30 @@
-import "./Button.css"
+// import "./Button.css"
+import styled from 'styled-components'
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  color: white;
+  background: #00358b;
+  border-radius: 4px;
+  box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
+  cursor: pointer;
 
-const Button = ({ type, children, disabled }) => {
-  return (
-    <button type={type} className="button" disabled={disabled}>
-      {children}
-    </button>
-  )
-}
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #245fbd;
+    box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
+  }
+`
+// const Button = ({ type, children, disabled }) => {
+  // return (
+    // <button type={type} className="button" disabled={disabled}>
+      /* {children} */
+    /* </button> */
+  // )
+// }
 
 export default Button
